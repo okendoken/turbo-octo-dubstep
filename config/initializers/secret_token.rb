3f68b44c9b4fd328bf919be6002e16b73c09c550
okendoken <philip.daineka@gmail.com>
2013-07-29 22:12:35 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freemarket::Application.config.secret_key_base = '299e977d78f002126f2aabf759f934a35c725c2b6202c9b98d80ef6c5484e0e743f47121f5ab74f43b76f66b1a0eadb30baf3dbcfa703054781c3fde42a26ba9'
