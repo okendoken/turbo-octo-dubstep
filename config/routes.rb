07e2c58463300fb686e53d1ec3838bb06edf6a30
okendoken <philip.daineka@gmail.com>
2013-07-29 23:58:37 +0300

    devise, bcrypt fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Freemarket::Application.routes.draw do
+  devise_for :users
+  root 'home#index'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
